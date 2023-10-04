c7315d090a59328b022d280fbee3eb45788a4df5
Hamza <110095038+HamzaHayee36@users.noreply.github.com>
2023-10-04 11:45:41 +0500

    Arithmetic Operators & Strings

---
+++ Arithmetic.py
@@ -0,0 +1,12 @@
+age1 = 21
+age2 = 23
+print("Total age is =",age1 + age2)
+print("Total age is =",age1 - age2)
+print("Total age is =",age1 / age2)
+print("Total age is =",age1 * age2)
+print("Total age is =",age1 % age2)
+
+
+
+
+
---
+++ Strings.py
@@ -0,0 +1,24 @@
+# Declaring a string variable 'Intro' with an introduction message
+Intro = "My name is Hamza, I'm a graduate student"
+
+# Printing the 'Intro' variable to the console
+print(Intro)
+
+# Re-declaring the 'Intro' variable with a newline character at the beginning
+Intro = "\nMy name is Hamza, I'm a graduate student"
+
+# Printing the 'Intro' variable 10 times with each instance on a new line
+print(Intro*10)
+
+# Declaring two string variables 'f_name' and 's_name' with first and second names respectively
+f_name = "Hamza"
+s_name = "Hayee"
+
+# Printing the concatenated version of 'f_name' and 's_name' with a space in between
+print(f_name + " " + s_name)
+
+# Declaring a string variable 'array' with a message about learning python
+array = "I'am learning python"
+
+# Printing a substring of 'array' starting from index 14 to 19 (20 is exclusive)
+print(array[14:20])
