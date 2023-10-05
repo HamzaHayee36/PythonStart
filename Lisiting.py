6a60ebe68ec5d5419d666ab59be99d81cd534ec4
Hamza <110095038+HamzaHayee36@users.noreply.github.com>
2023-10-05 21:46:00 +0500

    Listing basics completed

--- Lisiting.py
+++ Lisiting.py
@@ -13,3 +13,25 @@ print(shopping_list[0:4])
 # update in list
 shopping_list.append('LemonMalt')
 print(shopping_list)
+
+shopping_list[0]='orange'
+print(shopping_list)
+
+del shopping_list[0]
+print(shopping_list)
+
+#length of list
+print(len(shopping_list))
+
+#combining lists
+shopping_list2 = ['jam', 'mince','wings']
+print(shopping_list2)
+print(shopping_list+shopping_list2)
+
+#repeating a list
+print(shopping_list2*3)
+
+#min and max
+num_list = [1,8,5,3,6]
+print(max(num_list))
+print(min(num_list))
