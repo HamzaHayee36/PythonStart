b9d18f86617579d96ed352e801b0690de05e0958
Hamza <110095038+HamzaHayee36@users.noreply.github.com>
2023-10-05 21:37:08 +0500

    Listing

---
+++ Lisiting.py
@@ -0,0 +1,15 @@
+shopping_list = ['apple', 'banana', 'mango', 'burger']
+print(shopping_list)
+print(shopping_list[0])
+print(shopping_list[2])
+print(shopping_list[3])
+
+# slicing in list
+print(shopping_list[0:1])
+print(shopping_list[0:2])
+print(shopping_list[0:3])
+print(shopping_list[0:4])
+
+# update in list
+shopping_list.append('LemonMalt')
+print(shopping_list)
